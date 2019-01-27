2f9ceb553820106b4987c7a14b4f1829fcc1a15f
KTLiang <keith@hub-z.com>
2019-01-27 17:08:06 +1030

    Init

--- E70487/E70487/Program.cs
+++ E70487/E70487/Program.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Caching;
+using System.Collections.ObjectModel;
 
 namespace E70487
 {
+
+    public class MyMonitor : CacheEntryChangeMonitor
+    {
+        public override ReadOnlyCollection<string> CacheKeys => throw new NotImplementedException();
+
+        public override DateTimeOffset LastModified => throw new NotImplementedException();
+
+        public override string RegionName => throw new NotImplementedException();
+
+        public override string UniqueId => throw new NotImplementedException();
+
+        protected override void Dispose(bool disposing)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var cache = MemoryCache.Default;
+
+
+
         }
     }
 }
---
+++ E70487/E70487Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="E70487Web.MvcApplication" Language="C#" %>
